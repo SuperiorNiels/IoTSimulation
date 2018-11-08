436db9aaa2caae55d5b25c671697bc4ace4cd11f
Niels Balemans <balemans.niels@gmail.com>
2018-11-08 14:21:43 +0100

    update

--- Agent.py
+++ Agent.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 
-class Agent(object):
+class Agent():
 
     def __init__(self, values=np.zeros(3), neighbour_like=np.zeros(4)):
         assert(values.size == 3)
@@ -32,10 +32,40 @@ class Agent(object):
     def getColor(self):
         fav = self.getFavourite()
         if fav[0] == "ROCK":
-            return (255,0,0)
+            return (int(self.rock * 255) % 255,0,0)
         elif fav[0] == "POP":
-            return (0,255,0)
+            return (0,int(self.pop * 255) % 255,0)
         elif fav[0] == "TECHNO":
-            return (0,0,255)
+            return (0,0,int(self.techno * 255) % 255)
         # When we get here something went wrong, but we give a white color back
         return (255,255,255)
+
+    def update(self, neighbour_fav):
+        influence =  0.25 * neighbour_fav[1] + 0.25 * np.random.uniform(0,1)
+        if neighbour_fav[0] == "ROCK":
+            self.rock = 0.5 * self.rock + influence
+            self.pop = self.pop - influence * 0.5
+            self.techno = self.techno - influence * 0.5
+        elif neighbour_fav[0] == "POP":
+            self.rock = self.rock - influence * 0.5
+            self.pop = 0.5 * self.pop + influence
+            self.techno = self.techno - influence * 0.5
+        elif neighbour_fav[0] == "TECHNO":
+            self.rock = self.rock - influence * 0.5
+            self.pop = self.pop - influence * 0.5
+            self.techno = 0.5 * self.techno + influence
+
+        self.rock = max(0, self.rock)
+        self.pop = max(0, self.pop)
+        self.techno = max(0, self.techno)
+
+        if np.random.uniform(0,1,1) < .005:
+            values = np.random.uniform(0,1,3)
+            values = values / sum(values) if sum(values) != 0 else values
+            self.rock = values[0]
+            self.pop = values[1]
+            self.techno = values[2]
+            self.neighbour_like = np.random.uniform(0,1,4)
+
+
+
--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 import pygame
 import numpy.random as npr
 from Agent import Agent
+import random, time, json, copy, datetime, uuid
+from mqtt import mqtt
 
-sim_x = 50          # number of tiles on x as
+sim_x = 50        # number of tiles on x as
 sim_y = 50          # number of tiles on y as
 cell_width = 10     # 10 pixels per tile
+mqtt = mqtt("broker.hivemq.com",1883)
 
 Agent()
 # Create new cells
-cells = [[Agent(npr.uniform(0, 1, 3), npr.uniform(0, 1, 4)) for y in range(sim_x)] for x in range(sim_y)]
+cells = [[Agent(npr.uniform(0, 1, 3), npr.uniform(0, 1, 4)) for y in range(sim_y)] for x in range(sim_x)]
 
 def visualizeCells():
     for x in range(sim_x):
@@ -20,38 +23,61 @@ def visualizeCells():
     pygame.display.update()
 
 def updateCells():
+    global cells
+    buffer = copy.deepcopy(cells)
     for x in range(sim_x):
         for y in range(sim_y):
             neighbours = []
-            try: neighbours.append(cells[x - 1][y])
-            except IndexError: neighbours.append(Agent())
-            try: neighbours.append(cells[x + 1][y])
-            except IndexError: neighbours.append(Agent())
-            try: neighbours.append(cells[x][y - 1])
-            except IndexError: neighbours.append(Agent())
-            try: neighbours.append(cells[x][y + 1])
-            except IndexError: neighbours.append(Agent())
+            temp_x = x if x - 1 != -1 else 1
+            try: neighbours.append(cells[temp_x - 1][y])
+            except IndexError: pass
+            try: neighbours.append(cells[temp_x + 1][y])
+            except IndexError: pass
+            temp_y = y if y - 1 != -1 else 1
+            try: neighbours.append(cells[x][temp_y - 1])
+            except IndexError: pass
+            try: neighbours.append(cells[x][temp_y + 1])
+            except IndexError: pass
 
-            for neighbour in neighbours:
-                influence = 0.25 * neighbour.getFavourite()[1] + 0.25 * npr.uniform(0,1)
-                print(influence)
+            dir = []
+            for i in range(len(neighbours)): dir.append(i)
+            random.shuffle(dir)
+            for d in dir: buffer[x][y].update(neighbours[d].getFavourite())
+    cells = buffer
 
-
-
+def mqttCallback(msg):
+    print("MQTT message recieved")
+    msg = msg.decode("utf-8")
+    in_message = json.loads(msg)
+    genres = ["ROCK", "POP", "TECHNO"]
+    genre = genres[npr.randint(3)]
+    for row in cells:
+        for c in row:
+            res = {"timestamp": datetime.datetime.now().isoformat(),
+                   "value": -1,
+                   "usename": "IoTsim",
+                   "uid": str(uuid.uuid4()),
+                   "songid": in_message["songid"]}
+            if c.getFavourite()[0] == genre:
+                res["value"] = 1
+            mqtt.publish("votes", json.dumps(res))
 
 if __name__ == '__main__':
     (width, height) = (sim_x * cell_width, sim_y * cell_width)
     screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("IoT sim")
     pygame.display.flip()
 
-    running = True
+    mqtt.connect()
+    mqtt.add_listener_func(mqttCallback)
 
+    running = True
+    visualizeCells()
     while running:
-        # First check eventsx - 1
+        # First check events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         updateCells()
         visualizeCells()
-
